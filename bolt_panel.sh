e58abfb6b8605a08a7f4cd64f297e21219c459b8
Daniel Vela <veladan@me.com>
2023-01-06 11:34:11 +0100

    two monitors

--- bolt_panel.sh
+++ bolt_panel.sh
@@ -2,36 +2,36 @@
 
 osascript -e 'tell application "System Events" to tell process "Terminal"
     tell window 1
-        set position to {1850, 860}
-        set size to {1160, 840}
+        set position to {4810, 860}
+        set size to {1260, 840}
     end tell
 end tell'
 osascript -e 'tell application "Terminal" to do script "ssh -t danielvela@bolt watch sensors"'
 osascript -e 'tell application "System Events" to tell process "Terminal"
     tell window 1
-        set position to {1850, 400}
-        set size to {490, 330}
+        set position to {4810, 400}
+        set size to {530, 330}
     end tell
 end tell'
 osascript -e 'tell application "Terminal" to do script "ssh -t danielvela@bolt watch nvidia-smi"'
 osascript -e 'tell application "System Events" to tell process "Terminal"
     tell window 1
-        set position to {2330, 400}
+        set position to {5340, 400}
         set size to {680, 470}
     end tell
 end tell'
 osascript -e 'tell application "Terminal" to do script "ssh -t danielvela@bolt htop"'
 osascript -e 'tell application "System Events" to tell process "Terminal"
     tell window 1
-        set position to {1850, 0}
-        set size to {1160, 420}
+        set position to {4810, 0}
+        set size to {1260, 420}
     end tell
 end tell'
 osascript -e 'tell application "Terminal" to do script "ssh -t danielvela@bolt watch sudo liquidctl status"'
 osascript -e 'tell application "System Events" to tell process "Terminal"
     tell window 1
-        set position to {1850, 720}
-        set size to {490, 160}
+        set position to {4810, 720}
+        set size to {530, 160}
     end tell
 end tell'
 ssh bolt
